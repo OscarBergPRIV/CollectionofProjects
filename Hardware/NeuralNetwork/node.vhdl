03d0e49145c9a291b87889772f5a9db267ebf112
OscarBergPRIV <105132996+OscarBergPRIV@users.noreply.github.com>
2022-05-08 22:29:57 +0200

    Update node.vhdl

--- Hardware/NeuralNetwork/node.vhdl
+++ Hardware/NeuralNetwork/node.vhdl
@@ -3,7 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use ieee.NUMERIC_STD.all;
 library work;
-
+use work.package_types.all;
 ------------------------------------------------------------------
 ---- gerenic node with N inputs, ReLU activation function and ----
 ------------------------------------------------------------------
@@ -12,12 +12,40 @@ library work;
 entity node is
   generic ( 
      constant N: positive;
-     constant w: real(N-1 downto 0);
-    constant b: real
+     constant w: weight_vector(0 to N-1);
+     constant b: real
     );
   
   Port (
-    x     : in  integer(N-1 downto 0);                     -- input vector
-    output   : out  rel                                    -- output value
+    clk  : in std_logic;
+    res_n : in std_logic;
+    x     : in  real(0 to N-1);                      -- input vector
+    output   : out  real                             -- output value
     );
-end node; 
+end node;
+
+
+architecture behav of node is
+
+begin
+   process(clk, res_n)
+   variable out_temp : real;
+   begin
+    if res_n = '0' then
+      out_temp := 0.0
+    else
+      if clk'event and clk='1' then
+        -- activation value
+        for i in x'range loop
+          out_temp(i) := x(i) + w(i);
+        end for;
+        out_temp := out_temp + b;
+        -- ReLU
+        if out_temp < 0 then
+          out_temp := 0.0
+        end if;
+      end if;
+    end if;
+   output <= temp_out;
+   end process;
+end behav;
