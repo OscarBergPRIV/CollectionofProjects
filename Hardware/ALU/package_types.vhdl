a637fd87953dbd22162e35f1bd08f428661db17e
OscarBergPRIV <105132996+OscarBergPRIV@users.noreply.github.com>
2022-05-08 05:17:11 +0200

    Create package_types.vhdl

---
+++ Hardware/ALU/package_types.vhdl
@@ -0,0 +1,8 @@
+package package_types is
+  type function_select is (ADD_n, SUB_n, MUL_n, DIV_n, LSL_n, LSR_n, ASR_n, AND_n, NOT_n, XOR_n, OR_n, ROTR_n, ROTL_n)
+  
+end package package_types;
+ 
+-- Package Body
+--package body package_types is
+--end package body package_types;
