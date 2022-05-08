f0e029438d28021855c37d41e7f5b82f1827e9e0
OscarBergPRIV <105132996+OscarBergPRIV@users.noreply.github.com>
2022-05-08 05:48:44 +0200

    Update package_types.vhdl

--- Hardware/ALU/package_types.vhdl
+++ Hardware/ALU/package_types.vhdl
@@ -1,5 +1,5 @@
 package package_types is
-  type function_select is (ADD_n, SUB_n, MUL_n, DIV_n, LSL_n, LSR_n, ASR_n, AND_n, NOT_n, XOR_n, OR_n, ROTR_n, ROTL_n)
+  type function_select is (ADD_n, SUB_n, MUL_n, DIV_n, LSL_n, LSR_n, ASR_n, AND_n, NAND_n, NOR_n, XNOR_N, XOR_n, OR_n, ROTR_n, ROTL_n)
   
 end package package_types;
  
