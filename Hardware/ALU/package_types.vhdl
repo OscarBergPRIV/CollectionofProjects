5b73be6debcc4b3aa189582c24fc88cccf464ce0
OscarBergPRIV <105132996+OscarBergPRIV@users.noreply.github.com>
2022-05-08 22:07:52 +0200

    Update package_types.vhdl

--- Hardware/ALU/package_types.vhdl
+++ Hardware/ALU/package_types.vhdl
@@ -1,6 +1,6 @@
 package package_types is
   type function_select is (ADD_n, SUB_n, MUL_n, DIV_n, LSL_n, LSR_n, ASR_n, AND_n, NAND_n, NOT_n, NOR_n, XNOR_N, XOR_n, OR_n, ROTR_n, ROTL_n)
-  
+  type weight_vector is array(positive range <>) of real;
 end package package_types;
  
 -- Package Body
